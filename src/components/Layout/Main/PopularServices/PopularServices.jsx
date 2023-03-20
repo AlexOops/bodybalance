032434ca9c5e28c41402dac51547fd5891ad8ec6
Анатолий Шипилов <asmatol@yandex.ru>
2023-03-20 23:42:39 +0300

    added full main page

--- src/components/Layout/Main/PopularServices/PopularServices.jsx
+++ src/components/Layout/Main/PopularServices/PopularServices.jsx
@@ -1,5 +1,31 @@
+import {ArrowLeft} from "../../Arrow/ArrowLeft";
+import {ArrowRight} from "../../Arrow/ArrowRight";
+import s from './PopularServices.module.scss'
+import {Card} from "../../Card/Card";
+
+// при загрузке с сервера убрать эту переменную и изменить её ниже. пока для заглушки
+const price = 7200
+
 export const PopularServices = () => {
     return (
-        <div>популярные сервисы</div>
+        <>
+            <div className='container-carousel'>
+                <ArrowLeft/>
+                <div className='container'>
+                    <div className={s.position}>
+                        <div className={s.services}>
+                            <h2 className={s.space}>популярные услуги</h2>
+                            <div className={s.cards}>
+                                {/* изменить на получение с сервера пока только статика */}
+                                <Card price={price} />
+                                <Card />
+                                <Card />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ArrowRight/>
+            </div>
+        </>
     )
 }
