782cdf0873d44ef8c9af30dda38622b18133a1c2
Анатолий Шипилов <asmatol@yandex.ru>
2023-04-06 13:45:12 +0300

    added carusel to main page

--- src/components/Layout/Main/Slider/Dots/Dots.jsx
+++ src/components/Layout/Main/Slider/Dots/Dots.jsx
@@ -1,14 +1,14 @@
 import s from './Dots.module.scss'
 import {useState} from "react";
 
-export const Dots = () => {
+export const Dots = ({index, count, setCount}) => {
     const [active, setActive] = useState(false)
 
     const handleClick = () => {
-        setActive(!active)
+        setCount(index)
     }
 
     return (
-        <button onClick={handleClick} className={active ? `${s.dots} ${s.dotsActive}` : s.dots }></button>
+        <button onClick={handleClick} className={index === count ? `${s.dots} ${s.dotsActive}` : s.dots }></button>
     )
 }
