87acabcc35c4684d6f7c2ce6b480698047e59c23
Анатолий Шипилов <asmatol@yandex.ru>
2023-04-11 00:15:05 +0300

    refactor_main_carusel

--- src/components/Layout/Arrow/ArrowLeft.jsx
+++ src/components/Layout/Arrow/ArrowLeft.jsx
@@ -1,14 +1,15 @@
 import s from './Arrow.module.scss'
 import arrow from "../../../assets/Vector.svg";
+import {useEffect} from "react";
 
-export const ArrowLeft = ({setCount}) => {
+export const ArrowLeft = ({offset, setWidth, width}) => {
 
-    const changeCount = () => {
-        setCount(prev => prev - 1)
+    const handleClick = () => {
+        setWidth(width + offset)
     }
 
     return (
-        <div onClick={changeCount} className={`${s.arrowBackground} ${s.arrowPositionLeft}`}>
+        <div onClick={handleClick} className={`${s.arrowBackground} ${s.arrowPositionLeft}`}>
             <button className={`${s.arrow} ${s.rotate}`}><img className={s.size} src={arrow} alt=""/></button>
         </div>
     )
