86e36dc777339dedfa02574414a51fe44b48ffdc
Alex <8768905213@mail.ru>
2023-08-08 18:04:37 +0300

    customer creation request was created, the interface was added and the customer storage was changed

--- src/pages/Profile/ProfileMain/ProfileMain.jsx
+++ src/pages/Profile/ProfileMain/ProfileMain.jsx
@@ -1,11 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./ProfileMain.module.scss";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchPatientCards} from "../../../redux/slices/patientCard";
 import {fetchEmployers} from "../../../redux/slices/employers";
 import {fetchTraining} from "../../../redux/slices/training";
-import {logout} from "../../../redux/slices/auth";
-import {fetchCustomers} from "../../../redux/slices/customers";
+import {fetchCustomerList, fetchCustomers} from "../../../redux/slices/customers";
+import PhoneInput from "react-phone-number-input";
+import axios from "../../../axios";
 
 export const ProfileMain = () => {
     const dispatch = useDispatch();
@@ -16,12 +17,21 @@ export const ProfileMain = () => {
     const {training: videoCatalog} = useSelector(state => state.training);
     const {customers} = useSelector(state => state.customers);
 
+    const customer = customers.list.find((customer) => customer.userId._id === user._id);
+
     useEffect(() => {
         dispatch(fetchPatientCards())
         dispatch(fetchEmployers());
         dispatch(fetchTraining());
-        dispatch(fetchCustomers())
+        dispatch(fetchCustomerList())
     }, [dispatch]);
+
+    useEffect(() => {
+        if(customer) {
+            setPhone(customer.phone)
+            setDateOfBirth(customer.dateOfBirth)
+        }
+    }, [customer])
 
     // Карточка пациента
     const patientData = patients.items.find((patient) => patient.userId === user._id);
@@ -35,10 +45,57 @@ export const ProfileMain = () => {
     const trainingCatalog = patientData && patientData.catalogVideoId
         ? videoCatalog.items.find((catalog) => catalog._id === patientData.catalogVideoId)
         : null;
-
     const QuantityTrainingVideo = trainingCatalog ? trainingCatalog.videos.length : 0;
 
-    const customer = customers.items.find((customer) => customer._id.toString() === user.customer);
+    const [userId, serUserId] = useState(user._id);
+    const [fullName, setFullName] = useState(user.fullName);
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [email, setEmail] = useState(user.email);
+    const [phone, setPhone] = useState('');
+
+    const updateCustomer = async (data) => {
+        try {
+            const response = await axios.patch(`/customers/${user._id}`, data);
+            return response.data;
+        } catch (e) {
+            console.log("Ошибка обновления пользователя", e);
+            throw e;
+        }
+    }
+
+    const createCustomer = async (data) => {
+        try {
+            const response = await axios.post(`/customers`, data);
+            return response.data;
+        } catch (e) {
+            console.log("Ошибка создания пользователя", e);
+            throw e;
+        }
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        // Для проверки на существование, либо обновляем, либо создаем
+        const isExistingCustomer = customers.items.find((customer) => customer.userId === user._id);
+
+        const dataToSendInToCustomer = {
+            userId,
+            fullName,
+            phone,
+            email,
+            dateOfBirth
+        }
+
+        if (isExistingCustomer) {
+            const updatedCustomer = await updateCustomer(dataToSendInToCustomer);
+            console.log("Данные пользователя обновлены", updatedCustomer)
+        } else {
+            const newCustomer = await createCustomer(dataToSendInToCustomer);
+            console.log("Пользователь содан:", newCustomer)
+
+        }
+    }
 
     return (
         <>
@@ -46,15 +103,62 @@ export const ProfileMain = () => {
 
             <div className={s.container}>
                 <div className={s.healthId}>
-                    <p className={s.healthIdTitle}>Карточка пациента</p>
+                    <p className={s.healthIdTitle}>Моя карточка</p>
+
                     <img className={s.healthIdImg} src={user.avatarUrl} alt="customer"/>
-                    <p className={s.healthIdName}>{user.fullName}</p>
-                    <div className={s.healthIdWrp}>
-                        <p className={s.healthIdEmail}><span>Почта: </span>{user.email}</p>
-                        <p className={s.healthIdPhone}>
-                            <span>Номер телефона: </span>{(user.customer) && user.customer.phone}</p>
-                        <p className={s.healthIdBirth}><span>Дата рождения: </span>8 марта</p>
-                    </div>
+
+                    <form onSubmit={handleSubmit}>
+
+                        <label htmlFor="fullName"
+                               className={s.label}>
+                            Имя и фамилия:
+                            <input type="text"
+                                   className={s.input}
+                                   name={'fullName'}
+                                   value={fullName}
+                                   onChange={(e) => setFullName(e.target.value)}
+                            />
+                        </label>
+
+                        <label htmlFor="dateOfBirth"
+                               className={s.label}>
+                            Дата рождения:
+                            <input type="text"
+                                   className={s.input}
+                                   name={'dateOfBirth'}
+                                   value={dateOfBirth}
+                                   onChange={(e) => setDateOfBirth(e.target.value)}
+                            />
+                        </label>
+
+                        <label htmlFor="mail"
+                               className={s.label}>
+                            Почта:
+                            <input type="text"
+                                   className={s.input}
+                                   name={'mail'}
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </label>
+
+                        <label htmlFor="phone"
+                               className={s.label}>
+                            Номер телефона:
+                            <PhoneInput
+                                className={"phoneInputProfile"}
+                                international
+                                name={"phone"}
+                                defaultCountry="RU"
+                                value={phone}
+                                onChange={(value) => setPhone(value)}
+                            />
+                        </label>
+
+                        <button type={"submit"} className={s.button}>Сохранить</button>
+                    </form>
+
+
                 </div>
                 <div className={s.content}>
                     <div className={s.recommendations}>
--- src/redux/slices/customers.js
+++ src/redux/slices/customers.js
@@ -4,25 +4,30 @@ import staticData from "../staticData";
 
 export const fetchCustomers = createAsyncThunk('customers/fetchCustomers', async () => {
     const {data} = await axios.get('/customers/auth');
-    if(Array.isArray(data) && data.length > 0){
+    if (Array.isArray(data) && data.length > 0) {
         return data;
     }
     return staticData;
 });
 
+export const fetchCustomerList = createAsyncThunk('customers/fetchCustomerList', async () => {
+    const {data} = await axios.get('/profile');
+    console.log(data)
+    return data;
+});
+
 const initialState = {
     customers: {
-      items: [],
-      status: 'loading',
+        items: [],
+        status: 'loading',
+        list: []
     },
 };
 
 const customersSlice = createSlice({
     name: 'customers',
     initialState,
-    reducers: {
-        //
-    },
+    reducers: {},
     extraReducers: {
         [fetchCustomers.pending]: (state) => {
             state.customers.items = [];
@@ -34,6 +39,19 @@ const customersSlice = createSlice({
         },
         [fetchCustomers.rejected]: (state) => {
             state.customers.items = [];
+            state.customers.status = 'error';
+        },
+
+        [fetchCustomerList.pending]: (state) => {
+            state.customers.list = [];
+            state.customers.status = 'loading';
+        },
+        [fetchCustomerList.fulfilled]: (state, action) => {
+            state.customers.list = action.payload;
+            state.customers.status = 'loaded';
+        },
+        [fetchCustomerList.rejected]: (state) => {
+            state.customers.list = [];
             state.customers.status = 'error';
         },
     },
