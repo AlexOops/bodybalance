5d2de52567e2805497cc0ed8edad2f16d2b665ca
Hatallika <essenadmin@gmail.com>
2023-10-07 19:54:07 +0300

    fixed login  message, added register validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchAuth, selectIsAuth,} from "../../redux/slices/auth";
 import '../../index.scss'
@@ -7,12 +7,13 @@ import {useForm} from 'react-hook-form';
 import styles from "./Login.module.scss";
 import {closeModal, openModal} from "../../redux/slices/modal";
 import {Input} from "@mui/material";
+import Modal from "../../components/Modal/Modal";
 
 export const Login = () => {
     const isAuth = useSelector(selectIsAuth);
     const typeModalRegistration = 'modalRegister';
     const typeModalLogin = 'modalLogin';
-
+    const [authError, setAuthError] = useState(null)
     const dispatch = useDispatch();
     const {register, handleSubmit, formState: {errors, isValid}} = useForm({
         defaultValues: {
@@ -30,10 +31,9 @@ export const Login = () => {
         const data = await dispatch(fetchAuth(values));
 
         if(!data.payload){
-           return alert("Не удалось авторизоваться");
-        }
-
-        if ('token' in data.payload) {
+           // return alert("Не удалось авторизоваться");
+            setAuthError("Не удалось авторизоваться")
+        } else if ('token' in data.payload) {
             window.localStorage.setItem('token', data.payload.token); //сохраним токен в Localstorage
         }
     };
@@ -54,6 +54,14 @@ export const Login = () => {
                 <h2 className={styles.title}>
                     Вход
                 </h2>
+                <div className={"flexRelative"}>
+                    {
+                        (authError) &&
+                        <div className={`${styles.error} ${styles.errorMain}`}>
+                            {authError}
+                        </div>
+                    }
+                </div>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <div className={styles.fieldRow}>
                         <Input
